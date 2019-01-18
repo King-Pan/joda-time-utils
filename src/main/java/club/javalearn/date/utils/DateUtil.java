c64d5ec10587d92240b628c55d2419beee6270c0
king-pan <pwpw1218@gmail.com>
2019-01-18 18:03:26 +0800

    初始化提交

--- src/main/java/club/javalearn/date/utils/DateUtil.java
+++ src/main/java/club/javalearn/date/utils/DateUtil.java
@@ -2,10 +2,13 @@ package club.javalearn.date.utils;
 
 import org.joda.time.DateTime;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * @author king-pan
  * @date 2019/1/4
- * @Description  joda-time日期工具类
+ * @Description joda-time日期工具类
  */
 public class DateUtil {
 
@@ -20,7 +23,9 @@ public class DateUtil {
      */
     public static final String DEFAULT_TIME_FORMAT = "yyyyMMddHHmmss";
 
-    public static String getLastMonday(){
+    public static final String YYYY_MM_DD_HH_SS_MM = "yyyy-MM-dd HH:mm:ss";
+
+    public static String getLastMonday() {
         //获取当前日期
         DateTime dateTime = new DateTime();
         //获取本周日日期
@@ -30,7 +35,20 @@ public class DateUtil {
         return dateTime.toString(DEFAULT_DATE_FORMAT);
     }
 
-    public static String getCurrentTime(){
+
+    public static String parseDate(String dateFormat, String date, String format) {
+        String time = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        try {
+            DateTime dateTime = new DateTime(simpleDateFormat.parse(date).getTime());
+            time = dateTime.toString(format);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+    }
+
+    public static String getCurrentTime() {
         //获取当前时间
         DateTime now = new DateTime();
         return now.toString(DEFAULT_TIME_FORMAT);
@@ -40,5 +58,7 @@ public class DateUtil {
     public static void main(String[] args) {
         System.out.println(DateUtil.getLastMonday());
         System.out.println(DateUtil.getCurrentTime());
+
+        System.out.println(DateUtil.parseDate("yyyy-MM-dd HH:mm:ss","2019-01-08 01:31:03","yyyyMMddHHmmss"));
     }
 }
